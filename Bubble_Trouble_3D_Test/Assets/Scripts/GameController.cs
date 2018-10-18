6acdc07dda4b2edc888e7600af8a1b6505635eae
exura <henrikskiold@gmail.com>
2018-10-18 21:08:59 +0200

    Updated mouse look
    Changed so that the player cannot rotate in the y axis, only the camera can rotate y

---
+++ Bubble_Trouble_3D_Test/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text scoreText;
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+
+		SetText ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SetText() {
+
+		scoreText.text = "Score: " + score.ToString ();
+
+	}
+}
--- Bubble_Trouble_3D_Test/Assets/Scripts/camMouseLook.cs
+++ Bubble_Trouble_3D_Test/Assets/Scripts/camMouseLook.cs
@@ -3,12 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class camMouseLook : MonoBehaviour {
+	public enum RotationAxes {
+		MouseXAndY = 0,
+		MouseX = 1,
+		MouseY = 2
+	}
 
-	Vector2 mouseLook;
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+
+	public float sensitivityHor = 9.0f;
+	public float sensitivityVert = 9.0f;
+
+	public float minimumVert = -45.0f;
+	public float maximumVert = 45.0f;
+
+	private float _rotationX = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			// horizontal rotation here
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+		} else if (axes == RotationAxes.MouseY) {
+			// vertical rotation here
+			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+			_rotationX = Mathf.Clamp (_rotationX, minimumVert, maximumVert);
+
+			float rotationY = transform.localEulerAngles.y;
+
+			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+		} else {
+			// both horizontal and vertical rotation here
+			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+			_rotationX = Mathf.Clamp (_rotationX, minimumVert, maximumVert);
+
+			float delta = Input.GetAxis ("Mouse X") * sensitivityHor;
+			float rotationY = transform.localEulerAngles.y + delta;
+
+			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
+		}
+	}
+
+}
+
+	/*Vector2 mouseLook;
 	Vector2 smoothV;
 
 	public float sensitivity = 5.0f;
 	public float smoothing = 2.0f;
+
+	public float minVert = -45.0f;
+	public float maxVert = 45.0f;
 
 	GameObject character;
 
@@ -26,7 +71,6 @@ public class camMouseLook : MonoBehaviour {
 		smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1f / smoothing);
 		mouseLook += smoothV;
 
-		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+		transform.localRotation = Quaternion.AngleAxis (Mathf.Clamp(-mouseLook.y,minVert,maxVert), Vector3.right);
 		character.transform.localRotation = Quaternion.AngleAxis (mouseLook.x, character.transform.up);
-	}
-}
+	}*/
