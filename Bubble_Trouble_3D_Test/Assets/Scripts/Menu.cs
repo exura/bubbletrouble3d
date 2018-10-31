58adc8519a38183187209cca6a696f677111fb73
hipstiff <hipstiff@gmail.com>
2018-10-31 16:57:12 +0100

    COMMIT 41
    Main Menu
    Check Log

---
+++ Bubble_Trouble_3D_Test/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	public Canvas MainCanvas;
+	//Public Canvas HighscoresCanvas; /When done, just duplicate main canvas and change to fit.
+	//Public Canvas CreditsCanvas; /When done, just duplicate main canvas and change to fit.
+
+	void Awake()
+	{
+		//HighscoresCanvas.enabled = false;
+		//CreditsCanvas.enabled = false;
+	}
+
+	public void LoadOn()
+	{
+		Application.LoadLevel (1);
+	}
+
+
+}
